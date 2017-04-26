286fe00dddfe330cf5cfb5061d84b2e295dbc57e
PilotSSW <seanwolford@Seans-MacBook-Pro-3.local>
2017-04-26 14:16:08 -0400

    Initial Commit

---
+++ csv2bin.java
@@ -0,0 +1,123 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class csv2bin {
+
+    private static final char DEFAULT_SEPARATOR = ',';  // Split value
+    private static final char DEFAULT_QUOTE = '"';      // Quote sign
+    private static final int HEADER_ROW = 1;            // Row that contains the column headers
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<List<String>> csvGrid = new ArrayList<List<String>>();
+        String csvFile = args[0];
+        Scanner scanner = new Scanner(new File(csvFile));
+        while (scanner.hasNext()) {
+            List<String> line = parseLine(scanner.nextLine());
+            csvGrid.add(line);
+        }
+        scanner.close();
+
+        for(int i=0; i<csvGrid.get(HEADER_ROW).size(); i++){
+            for(int j=0; j<csvGrid.size(); j++){
+                if(csvGrid.get(j).get(i) != ""){
+                    System.out.println(csvGrid.get(j).get(i));
+                }
+            }
+            System.out.println("------------------------- Next Attribute -------------------------");
+        }
+    }
+
+    public static List<String> parseLine(String cvsLine) {
+        return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
+    }
+
+    public static List<String> parseLine(String cvsLine, char separators) {
+        return parseLine(cvsLine, separators, DEFAULT_QUOTE);
+    }
+
+    public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
+        List<String> result = new ArrayList<>();
+
+        //if empty, return!
+        if (cvsLine == null && cvsLine.isEmpty()) {
+            return result;
+        }
+
+        if (customQuote == ' ') {
+            customQuote = DEFAULT_QUOTE;
+        }
+
+        if (separators == ' ') {
+            separators = DEFAULT_SEPARATOR;
+        }
+
+        StringBuffer curVal = new StringBuffer();
+        boolean inQuotes = false;
+        boolean startCollectChar = false;
+        boolean doubleQuotesInColumn = false;
+
+        char[] chars = cvsLine.toCharArray();
+
+        for (char ch : chars) {
+
+            if (inQuotes) {
+                startCollectChar = true;
+                if (ch == customQuote) {
+                    inQuotes = false;
+                    doubleQuotesInColumn = false;
+                } else {
+
+                    //Fixed : allow "" in custom quote enclosed
+                    if (ch == '\"') {
+                        if (!doubleQuotesInColumn) {
+                            curVal.append(ch);
+                            doubleQuotesInColumn = true;
+                        }
+                    } else {
+                        curVal.append(ch);
+                    }
+
+                }
+            } else {
+                if (ch == customQuote) {
+
+                    inQuotes = true;
+
+                    //Fixed : allow "" in empty quote enclosed
+                    if (chars[0] != '"' && customQuote == '\"') {
+                        curVal.append('"');
+                    }
+
+                    //double quotes in column will hit this!
+                    if (startCollectChar) {
+                        curVal.append('"');
+                    }
+
+                } else if (ch == separators) {
+
+                    result.add(curVal.toString());
+
+                    curVal = new StringBuffer();
+                    startCollectChar = false;
+
+                } else if (ch == '\r') {
+                    //ignore LF characters
+                    continue;
+                } else if (ch == '\n') {
+                    //the end, break!
+                    break;
+                } else {
+                    curVal.append(ch);
+                }
+            }
+
+        }
+
+        result.add(curVal.toString());
+
+        return result;
+    }
+
+}
